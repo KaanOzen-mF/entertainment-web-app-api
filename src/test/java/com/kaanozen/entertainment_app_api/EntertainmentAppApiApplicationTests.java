f24e8a3d68a6bb4d3b9d0d78695a8ee9887eb897
KaanOzen-mF <kaanozn98@gmail.com>
2025-07-21 11:25:47 +0300

    Initialize Spring Boot application with basic structure and entities

---
+++ src/test/java/com/kaanozen/entertainment_app_api/EntertainmentAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaanozen.entertainment_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntertainmentAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
