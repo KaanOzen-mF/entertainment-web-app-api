e28d6f5e74b044284987acd592088affc54e627b
KaanOzen-mF <kaanozn98@gmail.com>
2025-07-25 17:15:02 +0300

    feat: Add CORS configuration to SecurityConfig for frontend access

--- src/main/java/com/kaanozen/entertainment_app_api/config/SecurityConfig.java
+++ src/main/java/com/kaanozen/entertainment_app_api/config/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -30,17 +35,32 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                // YENİ: Oluşturduğumuz AuthenticationProvider'ı Spring'e tanıtıyoruz.
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // Sadece frontend uygulamamızdan gelen isteklere izin veriyoruz.
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        // İzin verilen HTTP metotları (GET, POST, vb.)
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
+        // İzin verilen HTTP başlıkları
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // Bu kuralları projedeki tüm endpoint'ler ('/**') için geçerli yap.
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
